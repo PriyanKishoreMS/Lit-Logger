5baa42765eddb2e3d2f2d0a93941ee50d9e7f424
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-09-20 21:52:21 +0530

    npm link

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import Logger from "./Logger";
-import FileLogger from "./FileLogger";
-
-export { Logger, FileLogger };
--- utils/writeFile.ts
+++ utils/writeFile.ts
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import fs from "fs";
 import { fileSize } from "../utils";
 
 const writeFile = async (
